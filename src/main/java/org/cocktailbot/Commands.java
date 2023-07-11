13a8d62bfe02792e9416fccc90f9737bda56c056
Akej24 <aleks.skotarek.private@gmail.com>
2023-07-11 22:03:25 +0200

    Created first working command with hello and random drink

--- src/main/java/org/cocktailbot/Commands.java
+++ src/main/java/org/cocktailbot/Commands.java
@@ -3,6 +3,14 @@ package org.cocktailbot;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Commands extends ListenerAdapter {
 
@@ -10,8 +18,41 @@ public class Commands extends ListenerAdapter {
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         String message = event.getMessage().getContentRaw();
         String author = event.getAuthor().getName();
-        if(message.contains("run")){
-            event.getChannel().sendMessage("hello " + author).queue();
+
+        if (message.contains("drink") && !author.equals("Cocktail Bot")) {
+            URL url;
+            HttpURLConnection con;
+            BufferedReader in;
+            StringBuffer content = new StringBuffer();
+
+            try {
+                url = new URL("https://www.thecocktaildb.com/api/json/v1/1/random.php");
+                con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("GET");
+                in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    content.append(inputLine);
+                }
+
+                in.close();
+                con.disconnect();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            System.out.println(content);
+
+            JSONObject jsonObject = new JSONObject(content.toString());
+            JSONArray drinksArray = jsonObject.getJSONArray("drinks");
+            String strDrink = "";
+            if (drinksArray.length() > 0) {
+                JSONObject drinkObject = drinksArray.getJSONObject(0);
+                strDrink = drinkObject.getString("strDrink");
+            }
+
+            event.getChannel().sendMessage("Hello " + author + "!\nThis is your random drink: " + strDrink).queue();
         }
     }
 }
--- src/main/java/org/cocktailbot/Main.java
+++ src/main/java/org/cocktailbot/Main.java
@@ -2,6 +2,7 @@ package org.cocktailbot;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 
 import javax.security.auth.login.LoginException;
 
@@ -10,7 +11,10 @@ public class Main {
     private static final String TOKEN = "MTEyODM2OTI5MzIyMzAxMDQ0NQ.Gj_YKF.kB3IpuRGwwKIj5G01df9Z09-5ypJOlFOBsyE_8";
 
     public static void main(String[] args) throws LoginException {
-        JDA bot = JDABuilder.createDefault(TOKEN).build();
+        JDA bot = JDABuilder
+                .createDefault(TOKEN)
+                .setActivity(Activity.playing("Preparing drink"))
+                .build();
 
         bot.addEventListener(new Commands());
     }
