517498bf08e08f7eb4da848b6bd078ad2878a934
Akej24 <aleks.skotarek.private@gmail.com>
2023-07-12 16:42:07 +0200

    Divide Commands method into smaller methods and created UrlResponseReader class

--- src/main/java/org/cocktailbot/Commands.java
+++ src/main/java/org/cocktailbot/Commands.java
@@ -6,53 +6,28 @@ import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 public class Commands extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         String message = event.getMessage().getContentRaw();
         String author = event.getAuthor().getName();
-
-        if (message.contains("drink") && !author.equals("Cocktail Bot")) {
-            URL url;
-            HttpURLConnection con;
-            BufferedReader in;
-            StringBuffer content = new StringBuffer();
-
-            try {
-                url = new URL("https://www.thecocktaildb.com/api/json/v1/1/random.php");
-                con = (HttpURLConnection) url.openConnection();
-                con.setRequestMethod("GET");
-                in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-
-                String inputLine;
-                while ((inputLine = in.readLine()) != null) {
-                    content.append(inputLine);
-                }
-
-                in.close();
-                con.disconnect();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
-            System.out.println(content);
-
-            JSONObject jsonObject = new JSONObject(content.toString());
-            JSONArray drinksArray = jsonObject.getJSONArray("drinks");
-            String strDrink = "";
-            if (drinksArray.length() > 0) {
-                JSONObject drinkObject = drinksArray.getJSONObject(0);
-                strDrink = drinkObject.getString("strDrink");
-            }
-
-            event.getChannel().sendMessage("Hello " + author + "!\nThis is your random drink: " + strDrink).queue();
+        if (message.equalsIgnoreCase("!drink") && !author.equals("Cocktail Bot")) {
+            String randomCocktailJson = UrlResponseReader.getJsonResponseFromUrl("https://www.thecocktaildb.com/api/json/v1/1/random.php", "GET");
+            String drink = getCocktailNameFromJson(randomCocktailJson);
+            event.getChannel().sendMessage("Hello " + author + "!\nThis is your random drink: " + drink)./*addFile(new File("")).*/queue();
         }
     }
+
+    private static String getCocktailNameFromJson(String randomCocktailJsonResponse) {
+        JSONObject jsonObject = new JSONObject(randomCocktailJsonResponse);
+        JSONArray drinksArray = jsonObject.getJSONArray("drinks");
+        if (drinksArray.length() > 0) {
+            JSONObject drinkObject = drinksArray.getJSONObject(0);
+            return drinkObject.getString("strDrink");
+        }
+        return "not found";
+    }
+
+
 }
