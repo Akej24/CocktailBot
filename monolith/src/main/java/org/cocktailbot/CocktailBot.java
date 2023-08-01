0ff02cf2e636735aa661028defe04bef434f42ac
Akej24 <aleks.skotarek.private@gmail.com>
2023-08-01 02:16:04 +0200

    simplified add listeners

--- monolith/src/main/java/org/cocktailbot/CocktailBot.java
+++ monolith/src/main/java/org/cocktailbot/CocktailBot.java
@@ -1,6 +1,7 @@
 package org.cocktailbot;
 
 import net.dv8tion.jda.api.JDA;
+import org.cocktailbot.drink.command.favourite.FavouriteDrinkConfig;
 import org.cocktailbot.drink.command.ingredient.IngredientConfig;
 import org.cocktailbot.drink.command.recipe.DrinkRecipeConfig;
 import org.cocktailbot.drink.command.random.RandomDrinkConfig;
@@ -11,8 +12,11 @@ public class CocktailBot {
 
     public static void main(String[] args) throws LoginException {
         JDA bot = JdaBot.buildBot();
-        bot.addEventListener(RandomDrinkConfig.getInstance());
-        bot.addEventListener(DrinkRecipeConfig.getInstance());
-        bot.addEventListener(IngredientConfig.getInstance());
+        bot.addEventListener(
+                RandomDrinkConfig.getInstance(),
+                DrinkRecipeConfig.getInstance(),
+                IngredientConfig.getInstance(),
+                FavouriteDrinkConfig.getInstance()
+        );
     }
 }
