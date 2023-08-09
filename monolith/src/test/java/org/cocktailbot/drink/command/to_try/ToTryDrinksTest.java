47b001327ca9ce6c34a489eb55dc3afd9aea26d8
Akej24 <aleks.skotarek.private@gmail.com>
2023-08-09 18:57:59 +0200

    prepared project to creating tests

---
+++ monolith/src/test/java/org/cocktailbot/drink/command/to_try/ToTryDrinksTest.java
@@ -0,0 +1,13 @@
+package org.cocktailbot.drink.command.to_try;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ToTryDrinksTest {
+
+    @Test
+    public void from(){
+        assertTrue(true);
+    }
+}
